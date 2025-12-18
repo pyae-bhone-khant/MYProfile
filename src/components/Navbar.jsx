40c9c62f8d7b4d3532a8f9a9cdcbc805bc5ecacd
pyae-bhone-khant <pyaebhonen4@gmail.com>
2025-12-18 16:09:55 +0630

    Some

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           aria-label="Home"
           className="font-serif text-4xl font-bold text-white"
         >
-          PBK
+          Pyae Bhone Khant 
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
